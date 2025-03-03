207405962eb835e9540f836c5c73568e1816d7e4
auyaroglu <ardauyaroglu@hotmail.com>
2025-03-04 02:00:16 +0300

    pm2 kurulumu

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,26 @@ module.exports = {
   apps: [
     {
       name: 'carboninteraktif-seo',
-      script: 'server.js', // Oluşturduğumuz server.js dosyasını kullan
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
-        PORT: 3003
-      }
+        PORT: 3000,
+        API_KEY: process.env.API_KEY,
+        PAGESPEED_API_KEY: process.env.PAGESPEED_API_KEY
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      error_file: 'logs/error.log',
+      out_file: 'logs/output.log',
+      merge_logs: true
     }
   ]
 }; 
