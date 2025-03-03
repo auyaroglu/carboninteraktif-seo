137816c4785f1a5bb6f46d5c59bbd624cf40ac28
auyaroglu <ardauyaroglu@hotmail.com>
2025-03-04 02:01:46 +0300

    pm2 kurulumu

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,13 +10,13 @@ module.exports = {
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000,
+        PORT: 3003,
         API_KEY: process.env.API_KEY,
         PAGESPEED_API_KEY: process.env.PAGESPEED_API_KEY
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 3003
       },
       log_date_format: 'YYYY-MM-DD HH:mm Z',
       error_file: 'logs/error.log',
